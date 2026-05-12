80923c95f9e53045dd4c1836dda611b40088fd22
Sarvesh Mishra <sarveshmm99@gmail.com>
2026-05-13 00:58:59 +0530

    disabled the zoom of the website

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export const metadata: Metadata = {
 export const viewport: Viewport = {
   width: "device-width",
   initialScale: 1,
+  maximumScale: 1,
+  minimumScale: 1,
+  userScalable: false,
   themeColor: "#ffffff",
 };
 
